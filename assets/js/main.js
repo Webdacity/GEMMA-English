e7344c285a23a3c9403ce3437f304f373bee8b50
Arno van Staden <arnovanstaden@gmail.com>
2020-07-21 12:43:35 +0200

    update contact, about & concept page

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,19 @@ const closeNav = () => {
     // $(".navbar-content").css("width", "0");
     $(".navbar").removeClass("open");
     $(".navbar-page-wrapper").fadeOut(1000)
+}
 
+// Hide Navbar on Scroll
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-75px";
+    }
+    prevScrollpos = currentScrollPos;
 }
 
 // Footer
